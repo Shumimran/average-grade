9231050232eab8e49649ed6160a20acf6aaec080
Shumimran <164326671+Shumimran@users.noreply.github.com>
2024-06-04 11:49:46 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+//  Find the Average Grade: Given a list of grades, calculate the average grade.
+// Explain & TIP: We can sum up all the grades and divide by the number of grades to find the average, giving us 
+// an idea of how well someone did overall.
+let grade = [88, 94, 65, 96, 77];
+let averageGrade = grade.reduce((grade, total) => total + grade, 0) / grade.length;
+console.log(averageGrade);
